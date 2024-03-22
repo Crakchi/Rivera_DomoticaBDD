fb9e9a8e797d1c4ab60c7b184fcd9cf39e5e3eb3
Crakchi <crakchi@gmail.com>
2024-03-22 12:02:37 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Rivera_DomoticaBDD',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
